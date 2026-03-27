2a528a3389748daf5d4424b9e7366a739ce3c9c1
MujahidMalik7 <mujahidtufail726@gmail.com>
2026-03-27 11:58:21 +0500

    updated main.py for right call visualization

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from retriever import retrieve
 from generator import generate
 
 while True:
+    
     print ("=" * 50)
     query = input("\nQuery: ")
+    
     if not query.split():
         continue
+
     elif query in ["exit", "quit", "x", 'bye']:
         print("Goodbye!")
         break
     else:
         results = retrieve(query)
         response = (generate(query, results))
+        
         print ("\n")
         print ("=" * 50)
+        print ("Answer: ", response)
+        print ("\n")
+        
         print ("Sources: ")
         for r in results:
             print (f"- {r['source']} (chunk {r['chunk_id']})")
-        print ("Answer: ", response)
+        
         print ("="*50)
